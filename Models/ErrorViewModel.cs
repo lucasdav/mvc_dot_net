a27b1e48d80701dc7807d37b18a2855393e46254
lucasdav <lucassouzadavanso@gmail.com>
2023-09-18 17:10:23 -0300

    feature: project mvc using DotNet Core

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace mvc_dot_net.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
